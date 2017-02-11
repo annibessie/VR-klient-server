4a5ae4019b7756827f147d71a5e951ca79a23e10
Anni-Bessie Kitt <annibessie.kitt@gmail.com>
2017-02-11 18:21:53 +0200

    lisan PHP faili

---
+++ esimenekodukas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+	</head>
+	
+	<body>
+		<h1> Minu kodukas </h1>
+		
+		<h2> 2. katse luua kodukas </h2>
+		
+		<p> Lisan siia ka pildi. <p>
+		
+		<img src="lammas1.jpeg" alt="lammas" width="100" height="200">
+		
+	</body>
+</html>
+
+<?php
+echo 'Current PHP version: ' .phpversion();
+
+echo phpversion('tidy');
+?>
+
+
