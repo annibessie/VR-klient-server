dc0c91c5e17a8c43333c1d677af6507403796c82
Anni-Bessie Kitt <annibessie.kitt@gmail.com>
2017-03-04 16:32:57 +0200

    validaatoriga kinnitatud versioon

--- esimenekodukas.php
+++ esimenekodukas.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
+	<?php
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+	?>
 	<head>
+		<meta charset="utf-8" />
+		<title> Pealkiri </title>
 		<link rel="stylesheet" type="text/css" href="mystyle.css">
 	</head>
 	
@@ -13,13 +19,13 @@
 		
 		<img src="lammas1.jpeg" alt="lammas" width="100" height="200">
 		
-	</body>
-</html>
-
+		<div id="kuupaev"><script src="javascriptkodu.js"></script></div>
 <?php
 echo 'Current PHP version: ' .phpversion();
-
 echo phpversion('tidy');
 ?>
+	</body>
+	
+</html>
 
 
